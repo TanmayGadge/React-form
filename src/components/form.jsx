dd66493968ca7c6c07cfcb9ab58f5ed97b9cc6c2
Tanmay Gadge <tanmaygadge9273@gmail.com>
2024-06-30 20:55:47 +0530

    added word wrap

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,16 +1,15 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./form.css";
-
 
 function Form() {
   const [name, setName] = useState("");
   const [isName, setIsName] = useState(false);
   const [headingText, setHeading] = useState("");
 
-
   return (
     <>
       <div className="container">
+        
         <form
           onSubmit={(event) => {
             setHeading(name);
@@ -18,9 +17,12 @@ function Form() {
             event.preventDefault();
           }}
         >
-          <h1 className="heading">
-            Welcome <br /> {isName ? headingText : "User"}
-          </h1>
+          <div className="heading-container">
+            <h1 className="heading">
+              Welcome <br /> {isName ? headingText : "User"}
+            </h1>
+          </div>
+
           <input
             placeholder="Enter Your Name"
             onChange={(event) => {
